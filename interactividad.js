e5113410efe85d4fd44ba8a0304b6829ba18261b
MatiasIvanSanchez <122896904+MatiasIvanSanchez@users.noreply.github.com>
2023-04-13 18:41:53 -0300

    Add files via upload

---
+++ interactividad.js
@@ -0,0 +1,18 @@
+function doTask() {
+  alert("Gracias por estar acá");
+}
+
+document.getElementById('submit1').onclick = function () {
+  this.textContent = 'Para develar de lo que hacemos y los porqué';
+  
+}
+
+document.getElementById('submit2').onclick = function () {
+  this.textContent = 'Para expandir horizontes';
+  
+}
+
+document.getElementById('submit3').onclick = function () {
+  this.textContent = 'Para facilitar y recordar turnos';
+  
+}
